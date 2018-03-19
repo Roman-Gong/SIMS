799e1d29ba7c21dcadff4a7eb86b216246e1654b
GongYunHaoyyy <1501270490@qq.com>
2018-03-19 12:14:34 +0800

    first commit

---
+++ app/src/main/java/com/gongyunhao/sims/Bean/StudentBean.java
@@ -0,0 +1,85 @@
+package com.gongyunhao.sims.Bean;
+
+//    ┏┓　   ┏┓  
+// ┏━━┛┻━━━━━┛┻ ┓ 
+// ┃　　　　　　 ┃  
+// ┃　　　━　    ┃  
+// ┃　＞　　＜　 ┃  
+// ┃　　　　　　 ┃  
+// ┃... ⌒ ...  ┃  
+// ┃　　　　　 　┃  
+// ┗━━━┓　　　┏━┛  
+//     ┃　　　┃　  
+//     ┃　　　┃  
+//     ┃　　　┃  神兽保佑  
+//     ┃　　　┃  代码无bug　　  
+//     ┃　　　┃  
+//     ┃　　　┗━━━━━━━━━┓
+//     ┃　　　　　　　    ┣┓
+//     ┃　　　　         ┏┛
+//     ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+//       ┃ ┫ ┫   ┃ ┫ ┫
+//       ┗━┻━┛   ┗━┻━┛
+//
+//  作者：棒棒小糖
+//  來源：简书
+//
+//  Creste by GongYunHao on 2018/3/19
+// 
+public class StudentBean {
+
+    private String name;           //姓名
+    private boolean sex;           //性别
+    private String grade;          //年级
+    private String IDnumber;       //学号
+    private String major;          //专业
+
+    public StudentBean(String name, boolean sex, String grade, String IDnumber, String major) {
+        this.name = name;
+        this.sex = sex;
+        this.grade = grade;
+        this.IDnumber = IDnumber;
+        this.major = major;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getIDnumber() {
+        return IDnumber;
+    }
+
+    public void setIDnumber(String IDnumber) {
+        this.IDnumber = IDnumber;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
--- app/src/main/java/com/gongyunhao/sims/MainActivity.java
+++ app/src/main/java/com/gongyunhao/sims/MainActivity.java
@@ -3,7 +3,11 @@ package com.gongyunhao.sims;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private List<String> abc=new ArrayList<>(  );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
