0d41916c2958b3bf4c956b7bf8ac8c3f3a2e6167
GongYunHaoyyy <1501270490@qq.com>
2018-03-21 11:16:57 +0800

    save information is finished

--- app/src/main/java/com/gongyunhao/sims/Utils/FileHelper.java
+++ app/src/main/java/com/gongyunhao/sims/Utils/FileHelper.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.gongyunhao.sims.Bean.InterestCategoryAndHisIdBean;
+import com.gongyunhao.sims.Bean.InterestIdAndInterestNameAndInterestCategoryIdBean;
+import com.gongyunhao.sims.Bean.StudentAndInterestBean;
 import com.gongyunhao.sims.Bean.StudentBean;
 
 import java.io.BufferedReader;
@@ -47,7 +50,6 @@ public class FileHelper {
     private String LIKE_DATA="likeData";//学号对应的喜欢的情趣项的文件名
     private String BELONG_DATA="belongData";//存放兴趣编号和属于哪一个兴趣大类的文件名
     private String CATEGORY_DATA="categoryData";//存放兴趣大类的文件名
-
 
     /**
      * 读取学生信息完成。直接读出来成List。
@@ -122,6 +124,154 @@ public class FileHelper {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * 存储学生和兴趣信息完成。
+     * @param context
+     * @param sailist
+     */
+    //存储格式如下：
+    //201613137110 1
+    //201613137111 3
+    //201613137111 4
+    public void saveStudentAndInterestToFile(Context context, List<StudentAndInterestBean> sailist){
+        //save to File
+        FileOutputStream outputStream=null;
+        BufferedWriter writer=null;
+        try {
+            outputStream=context.openFileOutput( LIKE_DATA, Context.MODE_PRIVATE );//覆盖模式
+            writer=new BufferedWriter( new OutputStreamWriter( outputStream ) );
+            String studentdata =parseStudentAndInterestToSave( sailist );
+            writer.write( studentdata );
+        } catch (IOException e) {
+            e.printStackTrace( );
+        } finally {
+            try{
+                if (writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 存储兴趣id，兴趣名称，兴趣类别完成。
+     * @param context
+     * @param iainaicblist
+     */
+    //存储格式如下：
+    //1 足球 1
+    //2 篮球 1
+    //3 围棋 2
+    public void saveIISINSICIB(Context context, List<InterestIdAndInterestNameAndInterestCategoryIdBean> iainaicblist){
+        //save to File
+        FileOutputStream outputStream=null;
+        BufferedWriter writer=null;
+        try {
+            outputStream=context.openFileOutput( BELONG_DATA, Context.MODE_PRIVATE );//覆盖模式
+            writer=new BufferedWriter( new OutputStreamWriter( outputStream ) );
+            String studentdata =parseIISINSICIBBeanToSave( iainaicblist );
+            writer.write( studentdata );
+        } catch (IOException e) {
+            e.printStackTrace( );
+        } finally {
+            try{
+                if (writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 存储兴趣id和兴趣名称完成。
+     * @param context
+     * @param interestCategoryAndHisIdBeanList
+     */
+    //存储格式如下：
+    //1 球类
+    //2 棋类
+    public void saveInterestCategoryHisIdToFile(Context context, List<InterestCategoryAndHisIdBean> interestCategoryAndHisIdBeanList){
+        //save to File
+        FileOutputStream outputStream=null;
+        BufferedWriter writer=null;
+        try {
+            outputStream=context.openFileOutput( CATEGORY_DATA, Context.MODE_PRIVATE );//覆盖模式
+            writer=new BufferedWriter( new OutputStreamWriter( outputStream ) );
+            String studentdata =parseICAHI( interestCategoryAndHisIdBeanList );
+            writer.write( studentdata );
+        } catch (IOException e) {
+            e.printStackTrace( );
+        } finally {
+            try{
+                if (writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String parseICAHI(List<InterestCategoryAndHisIdBean> iCAHIdBList){
+
+//        interestCategoryName;
+//        interestCategoryId;
+
+        String datamydata ="";
+        for (int i=0;i<iCAHIdBList.size();i++){
+            InterestCategoryAndHisIdBean saiBean=iCAHIdBList.get( i );
+            if (i<iCAHIdBList.size()-1){
+                datamydata=datamydata+saiBean.getInterestCategoryId()+" "+ saiBean.getInterestCategoryName()+" "+saiBean.getInterestCategoryId()+"\n";
+            }else {
+                datamydata=datamydata+saiBean.getInterestCategoryId()+" "+ saiBean.getInterestCategoryName()+" "+saiBean.getInterestCategoryId();
+            }
+        }
+
+        Log.d( "saveICAHI------>",datamydata );
+        return datamydata;
+    }
+
+    private String parseIISINSICIBBeanToSave(List<InterestIdAndInterestNameAndInterestCategoryIdBean> miainaicblist){
+
+//        interestId;
+//        interestName;
+//        interestCategoryId;
+
+        String datamydata ="";
+        for (int i=0;i<miainaicblist.size();i++){
+            InterestIdAndInterestNameAndInterestCategoryIdBean saiBean=miainaicblist.get( i );
+            if (i<miainaicblist.size()-1){
+                datamydata=datamydata+saiBean.getInterestId()+" "+ saiBean.getInterestName()+" "+saiBean.getInterestCategoryId()+"\n";
+            }else {
+                datamydata=datamydata+saiBean.getInterestId()+" "+ saiBean.getInterestName()+" "+saiBean.getInterestCategoryId();
+            }
+        }
+
+        Log.d( "saveIISINSICIB------>",datamydata );
+        return datamydata;
+    }
+
+    private String parseStudentAndInterestToSave(List<StudentAndInterestBean> studentAndInterestBeanList){
+//        studentNumber;
+//        interestId;
+        String datamydata ="";
+        for (int i=0;i<studentAndInterestBeanList.size();i++){
+            StudentAndInterestBean saiBean=studentAndInterestBeanList.get( i );
+            if (i<studentAndInterestBeanList.size()-1){
+                datamydata=datamydata+saiBean.getStudentNumber()+" "+ saiBean.getInterestId()+"\n";
+            }else {
+                datamydata=datamydata+saiBean.getStudentNumber()+saiBean.getInterestId();
+            }
+        }
+
+        Log.d( "saveSAI---------->",datamydata );
+        return datamydata;
     }
 
     private String parseStudentBeanToSave(List<StudentBean> studentBeanList){
